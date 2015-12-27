1d30e0cdadf2cc08a5bb99acdad4926f1dfc2867
AlexeyUr <alisvcom@gmail.com>
2015-12-27 18:58:09 +0300

    1

---
+++ Lesson 14 HW 2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson 14 HW 2
+//
+//  Created by Alex on 27.12.15.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
